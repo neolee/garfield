f014552184569b3b519ca8b7cfe5156179462c9e
Neo Lee <neo.lee@gmail.com>
2026-01-27 15:35:32 +0800

    env: update mal and all packages

--- models.py
+++ models.py
@@ -5,11 +5,11 @@ deepseek = Model("deepseek/deepseek-chat", name="DeepSeek-V3")
 deepseek_reasoner = Model("deepseek/deepseek-reasoner", name="DeepSeek-R1")
 
 qwen = Model("qwen/qwen-plus-latest", name="Qwen Plus")
-qwen_max = Model("qwen/qwen3-max-preview", name="Qwen Max")
+qwen_reasoner = Model("qwen/qwen3-max-2026-01-23", name="Qwen Max")
 qwen_coder = Model("qwen/qwen3-coder-plus", name="Qwen-Coder Plus")
 
 kimi_k2 = Model("moonshot/kimi-k2-0905-preview", name="Kimi K2")
-kimi_reasoner = Model("moonshot/kimi-thinking-preview", name="Kimi Thinking")
+kimi_reasoner = Model("moonshot/kimi-2.5", name="Kimi K2.5")
 
 openrouter_gemini_flash = Model("openrouter/google/gemini-3-flash-preview", name="Gemini 3 Flash")
 openrouter_gemini_pro = Model("openrouter/google/gemini-3-pro-preview", name="Gemini 3 Pro")
