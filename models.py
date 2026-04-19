a989cdfec7250fe4d154c8afca9aeb0f4d207e07
Neo Lee <neo.lee@gmail.com>
2026-04-19 16:48:54 +0800

    env: update mal and packages

--- models.py
+++ models.py
@@ -4,16 +4,16 @@ from mal.adapter.openai import Model
 deepseek = Model("deepseek/deepseek-chat", name="DeepSeek-V3")
 deepseek_reasoner = Model("deepseek/deepseek-reasoner", name="DeepSeek-R1")
 
-qwen = Model("qwen")
+qwen = Model("qwen/qwen3.5-plus")
 
 kimi_k2 = Model("moonshot/kimi-k2-0905-preview", name="Kimi K2")
 kimi_reasoner = Model("moonshot/kimi-2.5", name="Kimi K2.5")
 
 gpt = Model("openai")
 
-local = Model("local/qwen3.5-nt", name="Qwen3.5-35B-A3B")
-local_reasoner = Model("local/qwen3.5", name="Qwen3.5-35B-A3B Thinking")
+local = Model("local/qwen3.6-nt", name="Qwen3.6-35B-A3B")
+local_reasoner = Model("local/qwen3.6", name="Qwen3.6-35B-A3B Thinking")
 
-omlx = Model("omlx", name="Gemma-4-26B")
+omlx = Model("omlx", name="Qwen3.6-35B-A3B")
 
-default = qwen
+default = omlx
